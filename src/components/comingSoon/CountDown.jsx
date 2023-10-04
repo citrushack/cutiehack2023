95c4008b01d008468748a449a4b4c8c5279f1eb4
menthy-wu <wumengxuan0728@gmail.com>
2023-10-04 01:05:46 -0700

    fix margin

--- src/components/comingSoon/CountDown.jsx
+++ src/components/comingSoon/CountDown.jsx
@@ -4,7 +4,7 @@ import { CONFIG } from "@/data/Config";
 
 const Digits = ({ time, text }) => {
   return (
-    <div className="flex flex-col items-center ml-3 md:ml-5 gap-2">
+    <div className="flex flex-col items-center mr-3 md:mr-5 gap-2">
       <div className="flex items-center">
         <p className="bg-cutie-blue-200/75 mb-0 h-10 md:h-16 text-lg md:text-3xl font-bold w-8 md:w-14 flex items-center justify-center rounded-lg drop-shadow-md mr-1 md:mr-2">
           {Math.floor(time[text] / 10)}
