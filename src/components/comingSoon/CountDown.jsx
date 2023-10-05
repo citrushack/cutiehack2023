3e855dc41f977376874fe7e2e541e25ab458337d
menthy-wu <wumengxuan0728@gmail.com>
2023-10-04 17:52:36 -0700

    fixes"

--- src/components/comingSoon/CountDown.jsx
+++ src/components/comingSoon/CountDown.jsx
@@ -2,15 +2,15 @@
 import { useState, useEffect } from "react";
 import { CONFIG } from "@/data/Config";
 
-const Digits = ({ time, text }) => {
+const Digits = ({ value, text }) => {
   return (
     <div className="flex flex-col items-center mr-3 md:mr-5 gap-2">
       <div className="flex items-center">
         <p className="bg-cutie-blue-200/75 mb-0 h-10 md:h-16 text-lg md:text-3xl font-bold w-8 md:w-14 flex items-center justify-center rounded-lg drop-shadow-md mr-1 md:mr-2">
-          {Math.floor(time[text] / 10)}
+          {Math.floor(value / 10)}
         </p>
         <p className="bg-cutie-blue-200/75 mb-0 h-10 md:h-16 text-lg md:text-3xl font-bold w-8 md:w-14 flex items-center justify-center rounded-lg drop-shadow-md">
-          {time[text] % 10}
+          {value % 10}
         </p>
       </div>
       <p className="text-xs md:text-base">{text}</p>
@@ -38,9 +38,9 @@ const CountDown = () => {
   return (
     time && (
       <div className="flex">
-        <Digits time={time} text="days" />
-        <Digits time={time} text="hours" />
-        <Digits time={time} text="minutes" />
+        {Object.entries(time).map(([text, value], index) => (
+          <Digits key={index} text={text} value={value} />
+        ))}
       </div>
     )
   );
