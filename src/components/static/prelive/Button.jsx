a916479b7b488712324bbfd3774ba3ac722835f9
seanquiambao <slquiambao03@gmail.com>
2023-10-12 13:43:14 -0700

    added static button

---
+++ src/components/static/prelive/Button.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { LuArrowUpRight } from "react-icons/lu";
+
+const Button = ({ text, onClick }) => {
+  const words = text.split(" ");
+  const last = words.pop();
+  return (
+    <div className="flex text-2xl font-bold items-center">
+      <p>
+        {words.join(" ")}{" "}
+        <span
+          className="text-cutie-orange cursor-pointer underline"
+          onClick={onClick}
+        >
+          {last}
+        </span>{" "}
+        <LuArrowUpRight className="inline" />
+      </p>
+    </div>
+  );
+};
+
+export default Button;
