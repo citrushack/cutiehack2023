a82b12532e5f37a4e1db225cc99b1b1f07e17086
seanquiambao <slquiambao03@gmail.com>
2023-10-13 12:48:54 -0700

    addressed comments

--- src/components/static/prelive/Button.jsx
+++ src/components/static/prelive/Button.jsx
@@ -1,21 +1,15 @@
-"use client";
 import { LuArrowUpRight } from "react-icons/lu";
 
 const Button = ({ text, onClick }) => {
-  const words = text.split(" ");
-  const last = words.pop();
   return (
     <div className="text-2xl font-bold items-center">
-      <p>
-        {words.join(" ")}{" "}
-        <span
-          className="text-cutie-orange cursor-pointer underline"
-          onClick={onClick}
-        >
-          {last}
-        </span>{" "}
-        <LuArrowUpRight className="inline" />
-      </p>
+      <span
+        className="text-cutie-orange cursor-pointer underline"
+        onClick={onClick}
+      >
+        {text}
+      </span>
+      <LuArrowUpRight className="inline" />
     </div>
   );
 };
