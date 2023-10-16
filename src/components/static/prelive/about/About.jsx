fa681534381b5380dfdf5fe5581e6c7f5b39231b
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-10-16 15:23:58 -0700

    addressed comments

--- src/components/static/prelive/about/About.jsx
+++ src/components/static/prelive/about/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
   return (
-    <div className="w-2/5 h-500 bg-cutie-blue-200/75 rounded-[73px] text-4xl font-bold p-5 leading-relaxed">
-      Cutie Hack is a <span className="text-cutie-orange">12-hour</span>,{" "}
-      <span className="text-cutie-orange">beginner-oriented hackathon</span>{" "}
+    <div className="w-2/5 h-500 bg-cutie-blue-200/75 rounded-3xl text-4xl font-bold p-5 leading-relaxed">
+      Cutie Hack is a <span className="text-cutie-orange">12-hour</span>,
+      <span className="text-cutie-orange"> beginner-oriented hackathon </span>
       hosted by students at University of California, Riverside where hackers
       are challenged to create a cool project within the time frame to demo in
       order to win awesome prizes and participate in workshops, fun games, and
