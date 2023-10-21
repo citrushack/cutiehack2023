6dc6df266195f0e6406a0251e270da5fad90e444
fardinzam <fardinzaman51@gmail.com>
2023-10-21 15:36:31 -0700

    forgot to remove accordion component from page

--- src/components/static/upcoming/Upcoming.jsx
+++ src/components/static/upcoming/Upcoming.jsx
@@ -5,7 +5,6 @@ import submarine from "../../../../public/svg/submarine.svg";
 import bubble from "../../../../public/svg/bubble.svg";
 import CountDown from "../Countdown";
 import Apply from "../prelive/Apply";
-import FAQ from "../prelive/faq/FAQ";
 
 const Upcoming = () => {
   return (
@@ -57,7 +56,6 @@ const Upcoming = () => {
         <div className="flex flex-col items-center w-fit">
           <Apply text="Participant" link="/forms/participants" />
         </div>
-        <FAQ />
       </div>
     </div>
   );
