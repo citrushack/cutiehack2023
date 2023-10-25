f21dc8a0cd9d07a2442492dc5b19f7fd4d45b46f
Sharan Singh <sharan.singh0203@gmail.com>
2023-10-25 16:20:06 -0700

    map through committee

--- src/components/static/prelive/team/Committees.jsx
+++ src/components/static/prelive/team/Committees.jsx
@@ -1,5 +1,19 @@
+import { COMMITTEES } from "@/data/static/Committees";
+
 const Committees = () => {
-  return <div>Committees</div>;
+  return (
+    <div>
+      <h2>Committees</h2>
+      {Object.entries(COMMITTEES).map(([committee, members], index) => (
+        <div key={index}>
+          <p>{committee}</p>
+          {members.map((member, memberIndex) => (
+            <p key={memberIndex}>{member}</p>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Committees;
