09a72c1090433adf582f7611c5d9222375019a6d
menthy-wu <wumengxuan0728@gmail.com>
2023-11-01 20:03:00 -0700

    populate from page

--- src/components/static/prelive/about/About.jsx
+++ src/components/static/prelive/about/About.jsx
@@ -1,12 +1,17 @@
+import SVG from "./SVG";
+
 const About = () => {
   return (
-    <div className="w-2/5 bg-cutie-blue-200/75 rounded-3xl text-4xl font-bold p-5 leading-relaxed">
-      Cutie Hack is a <span className="text-cutie-orange">12-hour</span>,
-      <span className="text-cutie-orange"> beginner-oriented hackathon </span>
-      hosted by students at University of California, Riverside where hackers
-      are challenged to create a cool project within the time frame to demo in
-      order to win awesome prizes and participate in workshops, fun games, and
-      networking.
+    <div className="flex flex-col md:flex-row items-center justify-center gap-8 w-10/12">
+      <SVG />
+      <div className="w-10/12 md:w-1/2 bg-cutie-blue-200/75 rounded-2xl text-lg md:text-2xl font-semibold p-[5%] leading-relaxed text-white">
+        Cutie Hack is a <span className="text-cutie-orange">12-hour</span>,
+        <span className="text-cutie-orange"> beginner-oriented hackathon </span>
+        hosted by students at University of California, Riverside where hackers
+        are challenged to create a cool project within the time frame to demo in
+        order to win awesome prizes and participate in workshops, fun games, and
+        networking.
+      </div>
     </div>
   );
 };
---
+++ src/components/static/prelive/landing/Landing.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../../../../public/svg/logo.svg";
+import shine from "../../../../../public/svg/shine.svg";
+import submarine from "../../../../../public/svg/submarine.svg";
+import bubble from "../../../../../public/svg/bubble.svg";
+import Countdown from "../../Countdown";
+import Apply from "../Apply";
+
+const Landing = () => {
+  return (
+    <div className="relative font-karla w-full text-white flex flex-col items-center justify-center bg-cutie-blue-300 mb-[8vh]">
+      <Image src={shine} alt="shine" className="absolute w-full inset-0 z-0" />
+      <div className="mt-[10vh] flex flex-col md:flex-row z-10 justify-center h-fit items-center md:items-stretch w-10/12 sm:w-5/6 2xl:w-2/3">
+        <div className="flex flex-col w-3/5 items-center md:items-start">
+          <Image
+            src={logo}
+            alt="logo"
+            className="w-9/12 sm:w-2/3 md:w-1/2 h-full object-contain"
+          />
+          <p className="text-base md:text-3xl text-cutie-orange font-bold my-1 md:my-3">
+            November 18th, 2023
+          </p>
+        </div>
+        <div className="hidden md:block w-2/5 h-full relative">
+          <Image
+            src={submarine}
+            alt="submarine"
+            className="w-9/12 h-full animate-jiggle-5.5s"
+          />
+          <Image
+            src={bubble}
+            alt="bubble"
+            className="absolute animate-jiggle-3s right-[15%] top-[40%] w-[5%]"
+          />
+          <Image
+            src={bubble}
+            alt="bubble"
+            className="absolute animate-jiggle-4s right-[5%] top-[30%] w-[8%]"
+          />
+          <Image
+            src={bubble}
+            alt="bubble"
+            className="absolute animate-jiggle-5s right-0 top-[50%] w-[12%]"
+          />
+        </div>
+      </div>
+      <div className="z-10 mt-4 mb-2 flex w-5/6 2xl:w-2/3 justify-center md:justify-start">
+        <Countdown />
+      </div>
+      <div className="z-10 mb-4 flex w-5/6 2xl:w-2/3 justify-center md:justify-start">
+        <div className="flex flex-col md:flex-row items-center gap-2 md:gap-3 w-fit">
+          <Apply text="Participate" link="/form/participant" />
+          <div className="flex items-center gap-3 w-fit">
+            <Apply text="Mentor" link="/form/mentor" />
+            <Apply text="Volunteer" link="/form/volunteer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
