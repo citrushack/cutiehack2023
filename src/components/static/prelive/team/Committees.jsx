371a207d2dfdd3dbbefb1b234c001730c3e5d074
menthy-wu <wumengxuan0728@gmail.com>
2023-11-01 18:58:17 -0700

    add more committee

--- src/components/static/prelive/team/Committees.jsx
+++ src/components/static/prelive/team/Committees.jsx
@@ -2,17 +2,21 @@ import { COMMITTEES } from "@/data/static/Committees";
 
 const Committees = () => {
   return (
-    <div>
-      <h2>Committees</h2>
-      {Object.entries(COMMITTEES).map(([committee, members], index) => (
-        <div key={index}>
-          <p>{committee}</p>
-          {members.map((member, memberIndex) => (
-            <p key={memberIndex}>{member}</p>
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <p className="text-4xl font-bold mb-5">Committees</p>
+      <div className="grid grid-cols-5 w-11/12">
+        {Object.entries(COMMITTEES).map(([committee, members], index) => (
+          <div key={index}>
+            <p className="text-2xl font-bold mb-3">{committee}</p>
+            {members.map((member, memberIndex) => (
+              <p className="mb-2" key={memberIndex}>
+                {member}
+              </p>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
