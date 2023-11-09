e980938c263596614e5b2c5658ae63690b7ed0e2
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-11-08 19:12:00 -0800

    better colorssss

--- src/components/dynamic/admin/dashboards/Modal.jsx
+++ src/components/dynamic/admin/dashboards/Modal.jsx
@@ -3,8 +3,8 @@ import { FaTimes } from "react-icons/fa";
 const Modal = ({ data, setModal }) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-1/2 h-3/4 rounded-xl">
-      <div className="flex justify-between items-center p-3 rounded-t-xl bg-cutie-orange">
-        <span className="text-3xl text-cutie-blue-200 font-bold">
+      <div className="flex justify-between items-center p-3 rounded-t-xl bg-cutie-blue-100">
+        <span className="text-2xl text-cutie-blue-300 font-bold">
           {data.title}
         </span>
         <FaTimes
