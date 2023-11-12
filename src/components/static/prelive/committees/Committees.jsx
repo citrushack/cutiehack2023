bf667e8a9f05caf5a22e279a1962b06e51ae196d
Divyank Shah <divyank.shah.2016@gmail.com>
2023-11-12 01:53:18 -0800

    fix

--- src/components/static/prelive/committees/Committees.jsx
+++ src/components/static/prelive/committees/Committees.jsx
@@ -1,26 +1,12 @@
 import Title from "../../Title";
-
-const committees = {
-  "UI/UX": ["Josalyn Chow", "Tenzin Zomkee", "Garman Zhen", "Kaitlin Ching"],
-  Operations: ["Allison Pham", "Akanksh Divyananda"],
-  Sponsorship: ["Brian Uong", "David Ranai"],
-  "Software Engineering": [
-    "Fardin Zaman",
-    "Sean Quiambao",
-    "Val Wong",
-    "Jonathan Trujillo",
-    "Fuyuki Malahom",
-    "Isabelle Celo",
-    "Anjaline Singh",
-  ],
-};
+import { COMMITTEES } from "@/data/static/Committees";
 
 const Committees = () => {
   return (
     <div className="flex justify-center items-center flex-col">
       <Title title="Committees" />
       <div className="grid grid-cols-1 md:grid-cols-4">
-        {Object.entries(committees).map(([key, values], index) => (
+        {Object.entries(COMMITTEES).map(([key, values], index) => (
           <div className="text-white text-center" key={index}>
             <p className="text-xl font-bold">{key}</p>
             {values.map((name, index) => (
