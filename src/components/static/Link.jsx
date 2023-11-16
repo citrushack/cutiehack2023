22cb57eb41060d9c72eedc2a805bfa227dca5e90
menthy-wu <wumengxuan0728@gmail.com>
2023-11-15 18:27:46 -0800

    add login

--- src/components/static/Link.jsx
+++ src/components/static/Link.jsx
@@ -4,9 +4,7 @@ const CustomLink = ({ link, text, onClick, icon }) => {
   return (
     <div className="bg-cutie-blue-200/75 text-cutie-orange font-bold text-base md:text-xl rounded-full px-8 py-1 w-fit hover:bg-cutie-blue-200 duration-200 whitespace-nowrap">
       {onClick ? (
-        <button onClick={onClick} href={link} target="_blank">
-          {text}
-        </button>
+        <button onClick={onClick}>{text}</button>
       ) : (
         <Link
           href={link}
